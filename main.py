82d5276b6c9164e4b8bffe74dc3068ed3e6a967e
Hiroki IKEDA <ikeda_hiroki@icloud.com>
2016-12-27 08:34:38 +0000

    Add allowed headers to preflight response.

--- main.py
+++ main.py
@@ -4,7 +4,10 @@ from funcs import d
 def app(environ, start_response):
     if 'POST'!=environ.get('REQUEST_METHOD'):
         if 'OPTIONS'==environ.get('REQUEST_METHOD'):
-            start_response('200 OK',[('Access-Control-Allow-Origin','*'), ('Access-Control-Allow-Methods', 'POST')])
+            start_response('200 OK',[
+                ('Access-Control-Allow-Origin','*'),
+                ('Access-Control-Allow-Methods', 'POST'),
+                ('Access-Control-Allow-Headers', 'Content-Type')])
             yield b''
         else:
             start_response('405 Method Not Allowed',[('Content-Type','text/plain')])
