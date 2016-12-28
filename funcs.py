b635af8751d2ee1bf0937197693f19f4e39e6e88
Hiroki IKEDA <ikeda_hiroki@icloud.com>
2016-12-28 15:37:50 +0000

    Enable multiple dot operations with just 1 request.

--- funcs.py
+++ funcs.py
@@ -4,9 +4,13 @@ import numpy as np
 import scipy as sp
 from scipy import linalg
 
+from functools import reduce
+
 @d.add_method
 def dot(*a, **k):
-    return np.dot(*a, **k).tolist()
+    alist = sorted(k)
+    a = list(a).extend(map(lambda key: k[key], alist))
+    return reduce(np.dot, a).tolist()
 
 @d.add_method
 def inv(*a, **k):
