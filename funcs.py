53052a41086674b6d70fd5a4259af19bfe9128aa
Hiroki IKEDA <ikeda_hiroki@icloud.com>
2017-01-01 10:09:37 +0000

    Remove unused functions.

--- funcs.py
+++ funcs.py
@@ -15,40 +15,5 @@ def dot(*a, **k):
     return reduce(np.dot, a).tolist()
 
 @d.add_method
-def inv(*a, **k):
-    return sp.linalg.inv(*a, **k).tolist()
-
-@d.add_method
 def solve(*a, **k):
     return sp.linalg.solve(*a, **k).tolist()
-
-@d.add_method
-def solve_banded(*a, **k):
-    return sp.linalg.solve_banded(*a, **k).tolist()
-    
-@d.add_method
-def solveh_banded(*a, **k):
-    return sp.linalg.solveh_banded(*a, **k).tolist()
-
-@d.add_method
-def solve_circulant(*a, **k):
-    return sp.linalg.solve_circulant(*a, **k).tolist()
-
-@d.add_method
-def solve_triangular(*a, **k):
-    return sp.linalg.solve_triangular(*a, **k).tolist()
-
-@d.add_method
-def solve_toeplitz(*a, **k):
-    for i,j in zip(a,('c', 'r', 'b', 'check_finite')):
-        k[j]=i
-    k['c_or_cr'] = (k.pop('c'), k.pop('r')) if 'r' in k else k.pop('c')
-    k['b'] = np.array(k['b'])
-    return sp.linalg.solve_toeplitz(**k).tolist()
-
-d.add_method(sp.linalg.det, 'det')
-d.add_method(sp.linalg.norm, 'norm')
-
-@d.add_method
-def matrix_rank(*a, **k):
-    return int(np.linalg.matrix_rank(*a, **k))
--- tests.py
+++ tests.py
@@ -1,7 +1,15 @@
 import unittest
 import json
 from jsonrpc import dispatcher as d
+import frame
 import funcs
+
+#class FrameTests(unittest.TestCase):
+#    def test_effectiveCoodinates(self):
+#        nodes = [{'recid':0,'x':-5.4,'y':3.2,'z':4.2},{'recid':1,'x':0,'y':-3.2,'z':1.5},{'recid':3,'x':-2.4,'y':-3.8,'z':9.9}]
+#        boundaries = [{'recid':0,'node':1,'x':100,'y':0,'z':False,'rx':-15,'ry':True,'rz':False},{'recid':2,'node':0,'x':0,'y':0,'z':True,'rx':False,'ry':False,'rz':1}]
+#        a = frame.effectiveCoodinates(nodes, boundaries)
+#        self.assertEqual(a, [(0,'x'),(0,'y'),(0,'rx'),(0,'ry'),(0,'rz'),(1,'x'),(1,'y'),(1,'z'),(1,'rx'),(1,'rz'),(3,'x'),(3,'y'),(3,'z'),(3,'rx'),(3,'ry'),(3,'rz')])
 
 class FuncTests(unittest.TestCase):
     def test_dot_array(self):
@@ -13,13 +21,7 @@ class FuncTests(unittest.TestCase):
     def test_dot_dict(self):
         a = d['dot'](a=[[1,2],[3,4]], b=[[1,3],[2,1]], c=[[2,1],[5,4]])
         self.assertEqual(a, [[35,25],[87,63]])
-        
-    def test_inv(self):
-        a = d['inv']([[1,0],[0,2]])
-        self.assertEqual(a, [[1,0],[0,0.5]])
-        #test json serializable
-        self.assertEqual(json.loads(json.dumps(a)), json.loads(json.dumps([[1,0],[0,0.5]])))
-        
+
     def test_solve_array(self):
         a = d['solve']([[1,2],[3,4]], [5,11])
         self.assertEqual(a, [1,2])
@@ -29,12 +31,6 @@ class FuncTests(unittest.TestCase):
     def test_solve_dict(self):
         a = d['solve'](a=[[1,2],[3,4]], b=[5,11])
         self.assertEqual(a, [1,2])
-        
-    def test_matrix_rank(self):
-        a = d['matrix_rank']([[1,2],[3,5]])
-        self.assertEqual(a, 2)
-        #test json serializable
-        self.assertEqual(json.dumps(a), json.dumps(2))
 
 if __name__ == '__main__':
     unittest.main()
