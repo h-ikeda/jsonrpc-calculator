a86b775e57df5a661284fce55519cd37a9d3452d
Hiroki IKEDA <ikeda_hiroki@icloud.com>
2017-01-11 05:25:50 +0000

    Simplify response.

--- main.py
+++ main.py
@@ -7,15 +7,14 @@ def app(environ, start_response):
             start_response('200 OK',[
                 ('Access-Control-Allow-Origin','*'),
                 ('Access-Control-Allow-Methods', 'POST'),
-                ('Access-Control-Allow-Headers', 'Content-Type')])
             yield b''
         else:
-            start_response('405 Method Not Allowed',[('Content-Type','text/plain')])
-            yield b'405 Method Not Allowed'
+            start_response('405 Method Not Allowed',[])
+            yield b''
     else:
         j=JSONRPCResponseManager.handle(environ['wsgi.input'].read().decode(), d)
         if j:
-            start_response('200 OK',[('Content-Type','application/json'), ('Access-Control-Allow-Origin','*')])
+            start_response('200 OK',[('Access-Control-Allow-Origin','*')])
             yield j.json.encode()
         else:
             start_response('204 No Content',[('Access-Control-Allow-Origin','*')])
