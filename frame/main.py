47172f71074eeed1dcadeace709329f2c3a2379b
Hiroki IKEDA <ikeda_hiroki@icloud.com>
2017-01-24 09:18:39 +0000

    improve code style.

--- frame/main.py
+++ frame/main.py
@@ -10,22 +10,12 @@
 import numpy as np
 from scipy.linalg import solve
 from . import matrix
+from . import model
 
 directions = ('x', 'y', 'z', 'rx', 'ry', 'rz')
-
-
-def effectiveCoodinates(nodes, boundaries):
-    t = {b['node']: {d for d in directions if isinstance(b[d], bool) and b[d]} for b in boundaries}
-    return tuple((n['recid'], d) for n in nodes for d in directions if n['recid'] not in t or d not in t[n['recid']])
-
-
-def itemById(items, recid):
-    for item in items:
-        if item['recid'] == recid:
-            return item
-
+effectiveCoodinates = model.effectiveCoodinates
+itemById = model.itemById
 transformMatrixLocalToGlobal = matrix.localToGlobalTransformer
-
 lineStiffnessGlobal = matrix.lineStiffnessGlobal
 
 
