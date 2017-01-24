b9e1bea36982ef194ea9a1cd7a2e7803a1aa8ec4
Hiroki IKEDA <ikeda_hiroki@icloud.com>
2017-01-24 08:10:50 +0000

    Improve code style.

--- frame/matrix.py
+++ frame/matrix.py
@@ -1,31 +1,32 @@
 import numpy as np
 from numpy.linalg import norm
 
-def localToGlobalTransformer(vectorZ, vectorX = None):
-    if np.allclose(vectorZ[:2],(0,0)):
-        t = tuple(i / t for i,t in zip(vectorX[:2], (norm(vectorX[:2]),)*2)) if vectorX is not None else (1,0)
+
+def localToGlobalTransformer(vectorZ, vectorX=None):
+    if np.allclose(vectorZ[:2], (0, 0)):
+        t = tuple(i / t for i, t in zip(vectorX[:2], (norm(vectorX[:2]),)*2)) if vectorX is not None else (1, 0)
         s = np.sign(vectorZ[2])
         return (
             (t[0], -s*t[1], 0) * 2,
             (t[1],  s*t[0], 0) * 2,
             (   0,       0, s) * 2
         ) * 2
-    len = norm(vectorZ)
+    length = norm(vectorZ)
     plen = norm(vectorZ[:2])
     if vectorX is not None:
-        z = vectoZ / norm(vectorZ)
+        z = vectoZ / length
         x = vectorX - np.dot(vectorX, z) * z
-        cZ0 = (np.transpose(x[:2],(1,0)) - vectorZ[:2])/plen/norm(x)
-        sZ0 = np.sqrt((1-cZ0)*(1+cZ0))
+        cZ0 = (np.transpose(x[:2],(1,0)) - vectorZ[:2]) / plen / norm(x)
+        sZ0 = np.sqrt((1 - cZ0) * (1 + cZ0))
         return (
-            (-vectorZ[1] / plen * cZ0 -vectorZ[0] / plen * vectorZ[2] / len * sZ0, vectorZ[1] / plen * sZ0 -vectorZ[0] / plen * vectorZ[2] / len * cZ0, vectorZ[0] / len)*2,
-            (vectorZ[0] / plen * cZ0 -vectorZ[1] / plen * vectorZ[2] / len * sZ0, -vectorZ[0] / plen * sZ0 -vectorZ[1] / plen * vectorZ[2] / len * cZ0, vectorZ[1] / len)*2,
-            (plen / len * sZ0, plen / len * cZ0, vectorZ[2] / len)*2
+            (-vectorZ[1] / plen * cZ0 -vectorZ[0] / plen * vectorZ[2] / length * sZ0, vectorZ[1] / plen * sZ0 -vectorZ[0] / plen * vectorZ[2] / length * cZ0, vectorZ[0] / length)*2,
+            (vectorZ[0] / plen * cZ0 -vectorZ[1] / plen * vectorZ[2] / length * sZ0, -vectorZ[0] / plen * sZ0 -vectorZ[1] / plen * vectorZ[2] / length * cZ0, vectorZ[1] / length)*2,
+            (plen / length * sZ0, plen / length * cZ0, vectorZ[2] / length)*2
         )*2
     return (
-        (-vectorZ[1] / plen, -vectorZ[0] / plen * vectorZ[2] / len, vectorZ[0] / len) * 2,
-        (vectorZ[0] / plen, -vectorZ[1] / plen * vectorZ[2] / len, vectorZ[1] / len) * 2,
-        (0, plen / len, vectorZ[2] / len) * 2
+        (-vectorZ[1] / plen, -vectorZ[0] / plen * vectorZ[2] / length, vectorZ[0] / length) * 2,
+        (vectorZ[0] / plen, -vectorZ[1] / plen * vectorZ[2] / length, vectorZ[1] / length) * 2,
+        (0, plen / length, vectorZ[2] / length) * 2
     ) * 2
 
 
@@ -33,36 +34,36 @@ def localToGlobalTransformer(vectorZ, vectorX = None):
 def lineStiffnessGlobal(vector, EA):
     L = norm(vector)
     K11 = np.array((
-        (0,0,0,0,0,0),
-        (0,0,0,0,0,0),
-        (0,0,-EA/L,0,0,0),
-        (0,0,0,0,0,0),
-        (0,0,0,0,0,0),
-        (0,0,0,0,0,0)
+        (0, 0, 0, 0, 0, 0),
+        (0, 0, 0, 0, 0, 0),
+        (0, 0, -EA/L, 0, 0, 0),
+        (0, 0, 0, 0, 0, 0),
+        (0, 0, 0, 0, 0, 0),
+        (0, 0, 0, 0, 0, 0)
     ))
     K12 = np.array((
-        (0,0,0,0,0,0),
-        (0,0,0,0,0,0),
-        (0,0,EA/L,0,0,0),
-        (0,0,0,0,0,0),
-        (0,0,0,0,0,0),
-        (0,0,0,0,0,0)
+        (0, 0, 0, 0, 0, 0),
+        (0, 0, 0, 0, 0, 0),
+        (0, 0, EA/L, 0, 0, 0),
+        (0, 0, 0, 0, 0, 0),
+        (0, 0, 0, 0, 0, 0),
+        (0, 0, 0, 0, 0, 0)
     ))
     K21 = np.array((
-        (0,0,0,0,0,0),
-        (0,0,0,0,0,0),
-        (0,0,EA/L,0,0,0),
-        (0,0,0,0,0,0),
-        (0,0,0,0,0,0),
-        (0,0,0,0,0,0)
+        (0, 0, 0, 0, 0, 0),
+        (0, 0, 0, 0, 0, 0),
+        (0, 0, EA/L, 0, 0, 0),
+        (0, 0, 0, 0, 0, 0),
+        (0, 0, 0, 0, 0, 0),
+        (0, 0, 0, 0, 0, 0)
     ))
     K22 = np.array((
-        (0,0,0,0,0,0),
-        (0,0,0,0,0,0),
-        (0,0,-EA/L,0,0,0),
-        (0,0,0,0,0,0),
-        (0,0,0,0,0,0),
-        (0,0,0,0,0,0)
+        (0, 0, 0, 0, 0, 0),
+        (0, 0, 0, 0, 0, 0),
+        (0, 0, -EA/L, 0, 0, 0),
+        (0, 0, 0, 0, 0, 0),
+        (0, 0, 0, 0, 0, 0),
+        (0, 0, 0, 0, 0, 0)
     ))
     t = np.array(localToGlobalTransformer(vector))
-    return (np.dot(np.dot(t,K11),t.T), np.dot(np.dot(t,K12),t.T)), (np.dot(np.dot(t,K21),t.T), np.dot(np.dot(t,K22),t.T))
+    return (np.dot(np.dot(t, K11), t.T), np.dot(np.dot(t, K12), t.T)), (np.dot(np.dot(t, K21), t.T), np.dot(np.dot(t, K22), t.T))
--- frame/tests/test_main.py
+++ frame/tests/test_main.py
@@ -1,8 +1,7 @@
 from unittest import TestCase
 import numpy as np
 import frame
-from frame import model
-from frame import matrix
+
 
 class FrameTests(TestCase):
     def test_effectiveCoodinates(self):
@@ -15,7 +14,7 @@ class FrameTests(TestCase):
         items = [{'recid':1,'data':True},{'recid':31,'data':24.8},{'recid':11,'data':12.8},{'recid':44,'data':'string'}]
         a = frame.model.itemById(items, 11)
         self.assertEqual(a, {'recid':11,'data':12.8})
-        
+
     def test_localToGlobalTransformer(self):
         a = frame.matrix.localToGlobalTransformer([0,0,2.85])
         self.assertTrue(np.allclose(a, ((1,0,0)*2,(0,1,0)*2,(0,0,1)*2)*2))
@@ -25,23 +24,23 @@ class FrameTests(TestCase):
         self.assertTrue(np.allclose(a, ((0,-3./5,4./5)*2,(1,0,0)*2,(0,4./5,3./5)*2)*2))
         a = frame.matrix.localToGlobalTransformer([0,-4,-3])
         self.assertTrue(np.allclose(a, ((1,0,0)*2,(0,-3./5,-4./5)*2,(0,4./5,-3./5)*2)*2))
-        
+
     def test_frame_calculate(self):
         model = {
-            'nodes':[
+            'nodes': [
                 {'recid':0,'x':0,'y':0,'z':0},
                 {'recid':1,'x':0,'y':0,'z':1}
             ],
             'lines':[
-                {'recid':0,'n1':0,'n2':1,'EA':1}
+                {'recid': 0, 'n1': 0, 'n2': 1, 'EA': 1}
             ],
-            'boundaries':[
-                {'recid':0,'node':0,'x':True,'y':True,'z':True,'rx':True,'ry':True,'rz':True},
-                {'recid':1,'node':1,'x':True,'y':True,'z':0,'rx':True,'ry':True,'rz':True}
+            'boundaries': [
+                {'recid': 0, 'node': 0, 'x': True, 'y': True, 'z': True, 'rx': True, 'ry': True, 'rz': True},
+                {'recid': 1, 'node': 1, 'x': True, 'y': True, 'z': 0, 'rx': True, 'ry': True, 'rz': True}
             ],
-            'nodeLoads':[
-                {'recid':0,'node':1,'x':0,'y':1,'z':1,'rx':0,'ry':0,'rz':-1}
+            'nodeLoads': [
+                {'recid': 0,'node': 1, 'x': 0, 'y': 1, 'z': 1, 'rx': 0, 'ry': 0, 'rz': -1}
             ]
         }
         a = frame.frame_calculate(model)
-        self.assertEqual(a, {'displacements':{1:{'z':1}}})
+        self.assertEqual(a, {'displacements': {1: {'z': 1}}})
