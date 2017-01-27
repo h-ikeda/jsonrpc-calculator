ac80e841c4344260c087b8f152550b9cb319c441
Hiroki IKEDA <ikeda_hiroki@icloud.com>
2017-01-27 08:45:22 +0000

    improve code style.

--- frame/tests/test_matrix.py
+++ frame/tests/test_matrix.py
@@ -5,11 +5,11 @@ from frame import matrix
 
 class MatrixTests(TestCase):
     def test_localToGlobalTransformer(self):
-        a = matrix.localToGlobalTransformer([0,0,2.85])
-        self.assertTrue(np.allclose(a, ((1,0,0)*2,(0,1,0)*2,(0,0,1)*2)*2))
+        a = matrix.localToGlobalTransformer([0, 0, 2.85])
+        self.assertTrue(np.allclose(a, ((1, 0, 0)*2, (0, 1, 0)*2, (0, 0, 1)*2)*2))
         a = matrix.localToGlobalTransformer([0,0,-1.2])
-        self.assertTrue(np.allclose(a, ((1,0,0)*2,(0,-1,0)*2,(0,0,-1)*2)*2))
-        a = matrix.localToGlobalTransformer([4,0,3])
-        self.assertTrue(np.allclose(a, ((0,-3./5,4./5)*2,(1,0,0)*2,(0,4./5,3./5)*2)*2))
-        a = matrix.localToGlobalTransformer([0,-4,-3])
-        self.assertTrue(np.allclose(a, ((1,0,0)*2,(0,-3./5,-4./5)*2,(0,4./5,-3./5)*2)*2))
+        self.assertTrue(np.allclose(a, ((1, 0, 0)*2, (0, -1, 0)*2, (0, 0, -1)*2)*2))
+        a = matrix.localToGlobalTransformer([4, 0, 3])
+        self.assertTrue(np.allclose(a, ((0, -3./5, 4./5)*2, (1, 0, 0)*2,(0, 4./5, 3./5)*2)*2))
+        a = matrix.localToGlobalTransformer([0, -4, -3])
+        self.assertTrue(np.allclose(a, ((1, 0, 0)*2, (0, -3./5, -4./5)*2, (0, 4./5, -3./5)*2)*2))
