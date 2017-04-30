0c34086142b46da5c4b1317e171ab99bd4133634
Hiroki IKEDA <ikeda_hiroki@icloud.com>
2017-04-30 14:59:20 +0900

    メソッド名とパラメータを変更しました。

--- tests/test_main.py
+++ tests/test_main.py
@@ -61,23 +61,29 @@ class ResponseTest(TestCase):
         result = requests.post('http://127.0.0.1:8080', json={
             'jsonrpc': '2.0',
             'id': '249teg25e',
-            'method': 'frame_calculate',
+            'method': 'frame.calculate',
             'params': {
-                'frameModel': {
-                    'nodes': [
-                        {'recid': 0, 'x': 0, 'y': 0, 'z': 0},
-                        {'recid': 1, 'x': 0, 'y': 0, 'z': 1}
-                    ],
-                    'lines': [
-                        {'recid': 0, 'n1': 0, 'n2': 1, 'EA': 1}
-                    ],
-                    'boundaries': [
-                        {'recid': 0, 'node': 0, 'x': True, 'y': True, 'z': True, 'rx': True, 'ry': True, 'rz': True},
-                        {'recid': 1, 'node': 1, 'x': True, 'y': True, 'z': 0, 'rx': True, 'ry': True, 'rz': True}
-                    ],
-                    'nodeLoads': [
-                        {'recid': 0, 'node': 1, 'x': 0, 'y': 1, 'z': 1, 'rx': 0, 'ry': 0, 'rz': -1}
-                    ]
+                'model': {
+                    'nodes': {
+                        'a': {'x': 0, 'y': 0, 'z': 0},
+                        'b': {'x': 0, 'y': 0, 'z': 1}
+                    },
+                    'lines': {
+                        'c': {'n1': 'a', 'n2': 'b', 'section': 'g', 'material': 'h'}
+                    },
+                    'sections': {
+                        'g': {'shape':'H', 'H': 1, 'B': 1.5, 'tw': 0.5, 'tf': 0.25}
+                    },
+                    'materials': {
+                        'h': {'E': 1, 'G': 1}
+                    },
+                    'boundaries': {
+                        'd': {'node': 'a', 'x': True, 'y': True, 'z': True, 'rx': True, 'ry': True, 'rz': True},
+                        'e': {'node': 'b', 'x': True, 'y': True, 'z': 0, 'rx': True, 'ry': True, 'rz': True}
+                    },
+                    'nodeLoads': {
+                        'f': {'node': 'b', 'x': 0, 'y': 1, 'z': 1, 'rx': 0, 'ry': 0, 'rz': -1}
+                    }
                 }
             }
         }).json()
@@ -86,7 +92,7 @@ class ResponseTest(TestCase):
             'id': '249teg25e',
             'result': {
                 'displacements': {
-                    '1': {
+                    'b': {
                         'z': 1
                     }
                 }
